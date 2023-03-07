e69bdbefcfcbb5b1d63594941d4c226d1ae46d6d
Radhames Capellán <85122601+radhamesc-capellan@users.noreply.github.com>
2023-03-07 10:40:57 -0500

    category_id fix in initModels snake_eye

--- src/models/initModels.js
+++ src/models/initModels.js
@@ -3,11 +3,11 @@ const Categories = require("./categoty.models");
 const Todos = require("./todo.models");
 
 const initModels = () => {
-  Users.hasMany(Todos, { foreignKey: "userId" });
-  Todos.belongsTo(Users, { foreignKey: "userId" });
+  Users.hasMany(Todos, { foreignKey: "user_id" });
+  Todos.belongsTo(Users, { foreignKey: "user_id" });
 
-  Categories.hasMany(Todos, { foreignKey: "categoryId" });
-  Todos.belongsTo(Categories, { foreignKey: "categoryId" });
+  Categories.hasMany(Todos, { foreignKey: "category_id" });
+  Todos.belongsTo(Categories, { foreignKey: "category_id" });
 };
 
 module.exports = initModels;

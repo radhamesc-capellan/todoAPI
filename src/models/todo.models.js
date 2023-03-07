a8c6e8c595fa4fb153372b7aaf509d1beaffd632
Radhames Capellán <85122601+radhamesc-capellan@users.noreply.github.com>
2023-03-07 10:08:07 -0500

    first commit

---
+++ src/models/todo.models.js
@@ -0,0 +1,35 @@
+const { DataTypes } = require("sequelize");
+const db = require("../utils/database");
+
+const Todos = db.define("todos", {
+  id: {
+    type: DataTypes.INTEGER,
+    primaryKey: true,
+    autoIncrement: true,
+  },
+  title: {
+    type: DataTypes.STRING(100),
+    allowNull: false,
+  },
+  description: {
+    type: DataTypes.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: DataTypes.INTEGER(1),
+    allowNull: false,
+    defaultValue: 1,
+  },
+  userId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: "user_id",
+  },
+  categotyId: {
+    type: DataTypes.INTEGER,
+    allowNull: false,
+    field: "category_id",
+  },
+});
+
+module.exports = Todos;
